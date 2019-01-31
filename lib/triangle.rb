c49e2f3809ecc9be4912e35eeab6a13d08a1547b
Robert McCary <robert.mccary@yahoo.com>
2019-01-31 17:31:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,16 +1,16 @@
 class Triangle
   attr_accessor :first, :second, :third
   def initialize(first, second, third)
-    @first=first 
-    @second=second 
-    @third=third 
-  end 
+    @first=first
+    @second=second
+    @third=third
+  end
 
   def kind(type)
-    returns type 
-  end 
+    returns type
+  end
 
   class TriangleError < StandardError
     def message
-    end 
+    end
 end
